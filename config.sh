8de89538a28b9c08c6bbe062dfd38d22bd68d24d
Nicholas Wengel <ninjaperson24119@gmail.com>
2020-01-18 20:30:47 -0700

    yarn

--- config.sh
+++ config.sh
@@ -4,3 +4,8 @@
 # nodejs
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
