effc0bfade1dd7fd498ebc1b0a9f60223651fa01
wagonhelm <jkf@ualberta.ca>
2020-02-16 23:59:00 -0700

    yippeee

--- catkin_ws/src/neuro_car/src/car.cpp
+++ catkin_ws/src/neuro_car/src/car.cpp
@@ -1,9 +1,9 @@
 #include <ros/ros.h>
 #include <neuro_car/sigint_handler.h>
 #include <std_msgs/Int32.h>
+#include <std_msgs/String.h>
 #include <math.h>
 #include <list>
-#include <neuro_car/gpio.h>
 #include <stdlib.h>
 
 // note that ROS handles the controlCallback in series, so mutexes are unnecessary
@@ -17,18 +17,26 @@ int maxClosedLen = 20;
 std::list<int> eyesBuffer, fatigueBuffer, attentionBuffer;
 std::list<int> closedBuffer={1,1,1,1,1,1,1,1,1,1};
 ros::Subscriber svmEyesSub, svmFatigueSub, svmAttentionSub;
-ros::Publisher eyesPub, fatiguePub, attentionPub;
-
-int leftForwardPin = PIN_19;
-int rightForwardPin = PIN_21;
+ros::Publisher eyesPub, fatiguePub, attentionPub, motorPub;
+std_msgs::String motorString;
 
 void eyesCallback(const std_msgs::Int32::ConstPtr& msg)
 {
     if (static_cast<int>(msg->data) == 1) {
         std::cout << "open" << std::endl;
+        if (motorString.data != "med") {
+            motorString.data = "med";
+            motorPub.publish(motorString);
+        }
+
     }
     else {
         std::cout << "closed" << std::endl;
+        std_msgs::String e;
+        if (motorString.data != "slow") {
+        motorString.data = "slow";
+        motorPub.publish(motorString);
+        }
     }
 
     eyesBuffer.push_back(static_cast<int>(msg->data));
@@ -65,20 +73,15 @@ void eyesCallback(const std_msgs::Int32::ConstPtr& msg)
 
     // Maintain buffer of eyes closed
 
-    if (closed_avg <= 0.2) {
+    if (closed_avg <= 0.1) {
         std::cout << " ----------- pull over ------------" << std::endl;
-        gpio_write(leftForwardPin, HIGH);
-        ros::Duration(0.4).sleep();
-        gpio_write(rightForwardPin, HIGH);
-        ros::Duration(1).sleep();
-        gpio_write(leftForwardPin, LOW);
-        ros::Duration(0.3).sleep();
-        gpio_write(rightForwardPin, LOW);
         std::cout << " ----------- SHOCKING ------------" << std::endl;
-        std::string shock = "curl https://maker.ifttt.com/trigger/yeet/with/key/RZTCKKK2BHHJSTskhqM2q";
-        const char *command = shock.c_str();
-        system(command);
-        ros::Duration(10).sleep();
+//        std::string shock = "curl https://maker.ifttt.com/trigger/yeet/with/key/RZTCKKK2BHHJSTskhqM2q";
+//        const char *command = shock.c_str();
+//        system(command);
+        motorString.data = "pullover";
+        motorPub.publish(motorString);
+        ros::Duration(1000).sleep();
         closedBuffer.pop_front();
         closedBuffer.pop_front();
         closedBuffer.pop_front();
@@ -91,17 +94,6 @@ void eyesCallback(const std_msgs::Int32::ConstPtr& msg)
         closedBuffer.push_back(1);
     }
 
-    if (e.data == 1) {
-        gpio_write(leftForwardPin, LOW);
-        gpio_write(rightForwardPin, LOW);
-
-    }
-
-    else {
-        gpio_write(leftForwardPin, LOW);
-        gpio_write(rightForwardPin, LOW);
-
-    }
 }
 
 void fatigueCallback(const std_msgs::Int32::ConstPtr& msg)
@@ -148,6 +140,7 @@ int main(int argc, char** argv) {
     handleInterrupts(argc, argv, "car", true);
     ros::NodeHandle priv_nh("~");
 
+
     // load params
     int fps = 0;
     if (!priv_nh.getParam("fps", fps)) {
@@ -155,13 +148,6 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // Initialize Pins
-    gpio_export(leftForwardPin);
-    gpio_export(rightForwardPin);
-    gpio_set_dir(leftForwardPin, OUTPUT);
-    gpio_set_dir(rightForwardPin, OUTPUT);
-    gpio_write(leftForwardPin, LOW);
-    gpio_write(rightForwardPin, LOW);
 
     // log
     ROS_INFO("Running at FPS: %d", fps);
@@ -170,6 +156,9 @@ int main(int argc, char** argv) {
     eyesPub = priv_nh.advertise<std_msgs::Int32>("eyes", 0);
     fatiguePub = priv_nh.advertise<std_msgs::Int32>("fatigue", 0);
     attentionPub = priv_nh.advertise<std_msgs::Int32>("attention", 0);
+    motorPub = priv_nh.advertise<std_msgs::String>("/jetbot_motors/cmd_str", 0);
+
+
 
     // subscriber
     svmEyesSub = priv_nh.subscribe<std_msgs::Int32>("/svm_eyes/detection", 0, eyesCallback);
